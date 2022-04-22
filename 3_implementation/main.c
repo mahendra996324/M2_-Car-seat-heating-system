6447b80615cf9763f05a45ca820a6e3833cfc1f8
mahendra996324 <83395513+mahendra996324@users.noreply.github.com>
2022-04-22 12:29:57 +0530

    Add files via upload

---
+++ 3_implementation/main.c
@@ -0,0 +1,26 @@
+/*
+ * mahendra1.c
+ *
+ * Created: 21-04-2022 12:12:39
+ * Author : mahendrajammigumpula
+ */ 
+
+#include <avr/io.h>
+#include"A1.h"
+#include"A2.h"
+#include"A3.h"
+#include<util/delay.h>
+int main(void)
+{
+	LCD_INITIALIZATION();
+	while(1)
+	{
+		A1(); //Activity 1
+		A2(); //Activity 2
+		A3(); //Activity 3
+	}
+	return 0;
+}
+
+
+
