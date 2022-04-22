3d59264ca33bdd7dfdbd9d1e6c52ee37bbe6e769
mahendra996324 <83395513+mahendra996324@users.noreply.github.com>
2022-04-22 12:01:37 +0530

    Add files via upload

---
+++ 3_implementation/inc/A1.h
@@ -0,0 +1,12 @@
+/*
+ * A1.h
+ *
+ * Created: 21-04-2022 12:15:05
+ *  Author: mahendrajammigumpula
+ */ 
+
+
+#ifndef _A1_h_
+#define _A1_h_
+void A1(void);
+#endif // _A1_h_
---
+++ 3_implementation/inc/A3.h
@@ -0,0 +1,12 @@
+/*
+ * A3.h
+ *
+ * Created: 21-04-2022 12:16:40
+ *  Author: mahendrajammigumpula
+ */ 
+
+
+#ifndef _A3_h_
+#define _A3_h_
+char A3();
+#endif // _A3_h_
